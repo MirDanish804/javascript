bf268c703825821d3f671f09f857ce58ebfaddf6
MirDanish804 <mirdani360@gmail.com>
2024-08-12 20:56:37 +0500

    Add files via upload

---
+++ java  1/script.js
@@ -0,0 +1,20 @@
+/* 
+alert ('error we are facing some issue1')
+let a=45;
+a=50
+a='Danish'
+console.log(a)
+let w=95;
+w=50/5
+a='Danish'
+ console.log(w)
+let d =prompt('retype your password')
+console.log(d)
+
+ */
+let a = prompt ('enter');
+let b = prompt ('number');
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
